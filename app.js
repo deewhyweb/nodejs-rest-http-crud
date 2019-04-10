58823e81d8c5af06dcc78eecca21577062bcd1ce
Philip Hayes <phayes@redhat.com>
2019-04-10 11:18:28 -0400

    Added liveness probe to check db

--- app.js
+++ app.js
@@ -27,7 +27,24 @@ const swaggerJSDoc = require("swagger-jsdoc");
 const app = express();
 
 const probe = require("kube-probe");
+
 const db = require("./lib/db");
+
+let livenessCallback = (req, res) => {
+  db.query("select now()", err => {
+    if (!err) {
+      res.writeHead(200);
+      res.end("OK");
+    } else {
+      console.log("liveness not ok");
+      res.writeHead(500);
+      res.end("not ok");
+    }
+  });
+};
+const probeOptions = {
+  livenessCallback: livenessCallback
+};
 var swaggerDefinition = {
   info: {
     // API informations (required)
@@ -70,11 +87,11 @@ app.use("/licenses", express.static(path.join(__dirname, "licenses")));
 app.use("/api", fruits);
 
 // Add a health check
-probe(app);
 
 db.init()
   .then(() => {
-    console.log("Database init'd");
+    console.log("Database init'd, starting probe");
+    probe(app, probeOptions);
   })
   .catch(error => {
     console.log(error);
